811074831f5d665b3dffe1ba61df8b5cf5a56da8
ok2ju <soulSpringg@gmail.com>
2022-12-12 19:54:11 +0300

    feature: Add toc

--- apps/docs/lib/mdx.ts
+++ apps/docs/lib/mdx.ts
@@ -3,6 +3,9 @@ import path from "path";
 // @ts-ignore
 import glob from "glob";
 import matter from "gray-matter";
+import { hasProperty } from "hast-util-has-property";
+import { toString } from "hast-util-to-string";
+import { visit } from "unist-util-visit";
 
 const ROOT_PATH = process.cwd();
 const CONTENT_PATH = path.join(ROOT_PATH, "content");
@@ -33,4 +36,39 @@ export const getMdxBySlug = async (basePath: string, slug: string) => {
     "utf8"
   );
   return source;
+};
+
+export const extractHeadings = ({ headings }: { headings: any }) => {
+  return (tree: any) => {
+    const toc: any = [];
+    const flatToc: any = [];
+
+    visit(tree, "element", (node) => {
+      if (hasProperty(node, "id")) {
+        const depth = parseInt(node.tagName.substring(1));
+        const entry = {
+          id: node.properties.id.toString(),
+          title: toString(node),
+          depth,
+          children: [],
+        };
+
+        flatToc.push(entry);
+
+        let parent = toc;
+
+        for (let i = flatToc.length - 1; i >= 0; i--) {
+          const current = flatToc[i] || {};
+
+          if ((current.depth || 0) < entry.depth) {
+            parent = current.children || [];
+            break;
+          }
+        }
+        parent.push(entry);
+      }
+    });
+
+    headings.push(...toc);
+  };
 };
--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,15 +1,9 @@
 import "../styles/globals.css";
 // import "@te/core/styles.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import { nav, menu } from "../constants/routes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout nav={nav} menu={menu}>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
